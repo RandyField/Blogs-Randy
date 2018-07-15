e8b6a48adaf3857680e67e6bc1b6153464dd4953
randyfield <287572291@qq.com>
2018-07-16 03:29:54 +0800

    just start

---
+++ blog/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+namespace blog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Create IWebHostBuilder
+            CreateWebHostBuilder(args)
+
+            //Create a app's IWebHost
+            .Build()
+
+            //Start
+            .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+
+                //Startup.cs-Startup class
+                // .UseStartup<Startup>();
+                .UseStartup<StartupDevelopment>();
+    }
+}
